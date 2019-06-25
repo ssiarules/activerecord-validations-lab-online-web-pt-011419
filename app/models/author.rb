2850957cde1335f279169fe97bce0cbc89f4b60f
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-25 01:50:16 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,6 @@
 class Author < ActiveRecord::Base
-  validates :name, presence: true
-  validates :phone_number, length: {is: 10}
-  validates :author, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :phone_number, length: {is: 10}  
 end
 
 author = Author.new
