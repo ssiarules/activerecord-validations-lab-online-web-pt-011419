663ccd64e547e75efc94f2265c7dd916675c4adb
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-25 02:03:37 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
-end
+   validates :title, presence: true
+   validates :content, length: { minimum: 250}
+   validates :summary, length: {maximum: 250}
+   validates :category, inclusion: %w(Fiction Non-Fiction)
+   validate :title_is_clickbaity
+
+
+def title_is_clickbaity
+   if !title.nil? && (!title.include?("Won't Believe" || "Secret" || "Top [/\d+/]" || "Guess"))
+     errors.add(:title_is_clickbaity, "Try again! Title does not meet clickbait-y standards!")
+   end
+ end
+ end
